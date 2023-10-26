4effa791fa9b8c15b687e60bbb9e72adba6e16ec
mphstar <bintang.simdig20@gmail.com>
2023-10-27 02:54:27 +0700

    fix: where valid is true

--- app/Http/Controllers/DatasetController.php
+++ app/Http/Controllers/DatasetController.php
@@ -8,18 +8,19 @@ use Illuminate\Http\Request;
 
 class DatasetController extends Controller
 {
-    public function detail_dataset($user, $id){
-        $data = Dataset::with(['paper', 'user'])->whereHas('user', function($query) use($user){
+    public function detail_dataset($user, $id)
+    {
+        $data = Dataset::with(['paper', 'user'])->whereHas('user', function ($query) use ($user) {
             $query->where('email', $user);
-        })->where('id_data', $id)->first();
+        })->where('id_data', $id)->where('valid', 1)->first();
 
-        
-        if($data){
+
+        if ($data) {
             $startDate = Carbon::parse($data->updated_at);
             $interval = $startDate->diffInDays(Carbon::now());
 
-            $popular = Dataset::with('user')->orderBy('download_count', 'desc')->limit(6)->get();
-            $newer = Dataset::with('user')->orderBy('created_at', 'desc')->limit(6)->get();
+            $popular = Dataset::with('user')->where('valid', 1)->orderBy('download_count', 'desc')->limit(6)->get();
+            $newer = Dataset::with('user')->where('valid', 1)->orderBy('created_at', 'desc')->limit(6)->get();
             return view('pages.DetailDataset', compact('data', 'interval', 'popular', 'newer'));
         }
 
