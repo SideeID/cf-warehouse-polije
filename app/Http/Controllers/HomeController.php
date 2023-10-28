3b12adb0a0d8704fcd038940e3e72bccf5a8497a
dimas <sandipratama164026@gmail.com>
2023-10-28 16:32:07 +0700

    add: filter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dataset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -9,6 +10,18 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.utama');
+        $newer = Dataset::with('user')
+            ->where('valid', 1)
+            ->orderBy('created_at', 'desc')
+            ->limit(6)
+            ->get();
+
+        $popular = Dataset::with('user')
+            ->where('valid', 1)
+            ->orderBy('download_count', 'desc')
+            ->limit(6)
+            ->get();
+            
+        return view('pages.utama', compact('newer', 'popular'));
     }
 }
